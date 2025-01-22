6c2efea0a9ed9b7b247d6723ef18016c0796bc83
Adam Kroon <adamkroon47@gmail.com>
2025-01-22 14:39:00 -0800

    intro exercises to rust bootcamp

---
+++ hello_world/target/debug/deps/hello_world-460d281280326324.d
@@ -0,0 +1,5 @@
+/Users/adamkroon/Desktop/github/rust-bootcamp/hello_world/target/debug/deps/hello_world-460d281280326324: src/main.rs
+
+/Users/adamkroon/Desktop/github/rust-bootcamp/hello_world/target/debug/deps/hello_world-460d281280326324.d: src/main.rs
+
+src/main.rs:
