ab77c8b8029e36f22f14cd2c8d7fa7d55360fc2f
Adam Kroon <adamkroon47@gmail.com>
2025-01-27 12:22:19 -0800

    completed modoule 1 exercises

---
+++ module 1/about_me/target/debug/about_me.d
@@ -0,0 +1,1 @@
+/Users/adamkroon/Desktop/github/rust-bootcamp/about_me/target/debug/about_me: /Users/adamkroon/Desktop/github/rust-bootcamp/about_me/src/main.rs
---
+++ module 1/about_me/target/debug/deps/about_me-cc15862f283728b3.d
@@ -0,0 +1,5 @@
+/Users/adamkroon/Desktop/github/rust-bootcamp/about_me/target/debug/deps/about_me-cc15862f283728b3: src/main.rs
+
+/Users/adamkroon/Desktop/github/rust-bootcamp/about_me/target/debug/deps/about_me-cc15862f283728b3.d: src/main.rs
+
+src/main.rs:
